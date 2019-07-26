d9c9aab5ef61a1489a6e782ed157d950e71669fa
Joey <yousef.joe175@gmail.com>
2019-07-26 14:42:44 +0200

    putting union data type

--- main/embloyee.c
+++ main/embloyee.c
@@ -6,10 +6,21 @@ typedef struct {
 	f32 salary;
 	u8 name[50];
 }Embloyee;
+typedef union{
+	u8 ID ;
+	u8 age;
+}embloyee;
+}
 void main(void){
- Embloyee Ahmed = {123, 1200};
+	embloyee ahmed;
+	ahmed.ID=5;
+	printf("ahmed's ID= %d\n",ahmed.ID);
+	ahmed.age=22;
+	printf("ahmed's age= %d\n",ahmed.age);
+Embloyee Ahmed = {123, 1200};
 Embloyee *ptr = &Ahmed;
 strcpy(Ahmed.name, "Ahmed");
 printf("%.2f\n",ptr->salary);
 printf(Ahmed.name);
+
 }
